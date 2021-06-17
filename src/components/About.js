06d5637daeeac9c8f2eaba94b95c07b15d3c8477
Hafise Nur Bacaksız <hafisenur.bacaksiz@gmail.com>
2021-06-17 15:49:58 +0300

    done

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>about me</h2>
+    <p>adasrgvsafrgsfv</p>
+    <img src={image} alt="I made this "></img>
+  </div>;
 }
 
 export default About;
